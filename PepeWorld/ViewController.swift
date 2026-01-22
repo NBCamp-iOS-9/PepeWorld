bec477219d8fc056bb34156ac769e6d2f3063431
Jaesung Jung <jaesung.jung1986@gmail.com>
2026-01-22 20:15:04 +0900

    Implements collection view

--- PepeWorld/ViewController.swift
+++ PepeWorld/ViewController.swift
@@ -6,9 +6,110 @@
 //
 
 import UIKit
+import SnapKit
+import Kingfisher
+import Then
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+  private lazy var collectionView = UICollectionView(
+    frame: .zero,
+    collectionViewLayout: makeCollectionViewLayout()
+  )
+
+  private lazy var dataSource = makeCollectionViewDataSource(collectionView)
+
   override func viewDidLoad() {
     super.viewDidLoad()
+    title = "Pepe World"
+
+    view.addSubview(collectionView)
+    collectionView.snp.makeConstraints {
+      $0.directionalEdges.equalToSuperview()
+    }
+
+    Task {
+      do {
+        let images = try await ImageLoader().images()
+        var snapshot = NSDiffableDataSourceSnapshot<Int, ImageLoader.ImageItem>()
+        snapshot.appendSections([0])
+        snapshot.appendItems(images, toSection: 0)
+        await dataSource.apply(snapshot)
+      } catch {
+        print(error)
+      }
+    }
+  }
+}
+
+extension ViewController {
+  private func makeCollectionViewLayout() -> UICollectionViewLayout {
+    UICollectionViewCompositionalLayout { _, environment in
+      let contentSize = environment.container.effectiveContentSize
+
+      let size = (contentSize.width - 50) * 0.5
+      let group = NSCollectionLayoutGroup.horizontal(
+        layoutSize: NSCollectionLayoutSize(
+          widthDimension: .fractionalWidth(1),
+          heightDimension: .absolute(size)
+        ),
+        repeatingSubitem: NSCollectionLayoutItem(
+          layoutSize: NSCollectionLayoutSize(
+            widthDimension: .absolute(size),
+            heightDimension: .absolute(size)
+          )
+        ),
+        count: 2
+      ).then {
+        $0.interItemSpacing = .fixed(10)
+        $0.contentInsets = NSDirectionalEdgeInsets(top: 0, leading: 20, bottom: 0, trailing: 20)
+      }
+
+      return NSCollectionLayoutSection(group: group).then {
+        $0.interGroupSpacing = 10
+        $0.contentInsets = NSDirectionalEdgeInsets(top: 20, leading: 0, bottom: 20, trailing: 0)
+      }
+    }
+  }
+
+  private func makeCollectionViewDataSource(_ collectionView: UICollectionView) -> UICollectionViewDiffableDataSource<Int, ImageLoader.ImageItem> {
+    let cellRegistration = UICollectionView.CellRegistration<ImageCell, ImageLoader.ImageItem> { cell, indexPath, image in
+      cell.configure(with: image)
+    }
+    return UICollectionViewDiffableDataSource(collectionView: collectionView) { collectionView, indexPath, item in
+      collectionView.dequeueConfiguredReusableCell(using: cellRegistration, for: indexPath, item: item)
+    }
+  }
+}
+
+extension ViewController {
+  final class ImageCell: UICollectionViewCell {
+    let imageView = UIImageView().then {
+      $0.contentMode = .scaleAspectFill
+      $0.clipsToBounds = true
+      $0.layer.cornerRadius = 8
+    }
+
+    override init(frame: CGRect) {
+      super.init(frame: frame)
+      contentView.addSubview(imageView)
+      imageView.snp.makeConstraints {
+        $0.directionalEdges.equalToSuperview()
+      }
+    }
+
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+      fatalError("init(coder:) has not been implemented")
+    }
+
+    func configure(with image: ImageLoader.ImageItem) {
+      imageView.kf.setImage(with: image.thumbnail)
+    }
+  }
+}
+
+#Preview {
+  UINavigationController(rootViewController: ViewController()).then {
+    $0.navigationBar.prefersLargeTitles = true
   }
 }
