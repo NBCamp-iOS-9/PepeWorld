9b98107ea10d45afd8adf3ff8cc7a51ec673767a
hyunjin <hyunjinking2323@gmail.com>
2026-01-23 15:13:53 +0900

    재성성
    git hub

---
+++ PepeWorld/commit.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  PepeWorld
+//
+//  Created by 정재성 on 1/22/26.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    return true
+  }
+
+  // MARK: UISceneSession Lifecycle
+
+  func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+    return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+  }
+}
